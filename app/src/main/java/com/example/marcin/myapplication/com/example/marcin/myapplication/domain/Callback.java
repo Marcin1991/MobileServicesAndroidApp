75956b1233db83ce096d6708b2d1d3e8345b6cbc
Marcin Olszewski <olszewskimar.pg@gmail.com>
2015-12-14 20:56:20 +0100

    Offline and conflicts resolution.

---
+++ app/src/main/java/com/example/marcin/myapplication/com/example/marcin/myapplication/domain/Callback.java
@@ -0,0 +1,10 @@
+package com.example.marcin.myapplication.com.example.marcin.myapplication.domain;
+
+/**
+ * Created by marcin on 11.12.15.
+ */
+public interface Callback {
+
+    public void apply();
+
+}
---
+++ app/src/main/java/com/example/marcin/myapplication/com/example/marcin/myapplication/domain/ProductsCallback.java
@@ -0,0 +1,12 @@
+package com.example.marcin.myapplication.com.example.marcin.myapplication.domain;
+
+import java.util.List;
+
+/**
+ * Created by marcin on 07.11.15.
+ */
+public interface ProductsCallback {
+
+    public void apply(List<Product> products);
+
+}
